18892886b047517791e283be9a433f92cf35d73c
sauravbedi <sauravbedi@10.48.68.142>
2024-05-13 13:59:11 -0400

    initial commit

---
+++ src/studentlist/StudentList.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package studentlist;
+
+/**
+ *
+ * @author sauravbedi
+ */
+public class StudentList {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Student[] studentList = new Student[3];
+        
+        Student s1 = new Student();
+        s1.setName ("Saurav Bedi");
+        
+        Student s2 = new Student();
+        s2.setName ("Sahil Bedi");
+        
+        Student s3 = new Student();
+        s3.setName ("kiran");
+        
+        studentList[0] = s1;
+        studentList[1] = s2;
+        studentList[2] = s3;
+        
+        for (Student s : studentList){
+            System.out.println(s.getName());
+        }
+    }
+    
+}
